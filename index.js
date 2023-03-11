0cbafc5fe56f1c79d81bec7987b141c1bb3e2b9a
Rafael Finol <rafaelfinol88@gmail.com>
2023-03-10 23:32:28 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+        cats.push("Ralph");
+         //Another way
+        //cats.splice(3, 0,"Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+   const copyCat = [...cats, "Broom"];
+   return copyCat;
+}
+
+function prependCat(){
+    const copyCat = ["Arnold", ...cats];
+    return copyCat;
+ }
+
+ function removeLastCat(){
+    const copyCat = cats.slice(0, cats.length - 1);
+    return copyCat;
+ }
+
+ function removeFirstCat(){
+    const copyCat = cats.slice(1);
+    return copyCat;
+ }
